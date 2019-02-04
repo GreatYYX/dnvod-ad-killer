eed5ccc5808403337b90afae56d3edc0a218024c
greatyyx <bigyyx@gmail.com>
2019-02-03 19:24:17 -0800

    update to adapt new js

--- call_killer.py
+++ call_killer.py
@@ -16,8 +16,8 @@ class DNAdKiller:
                 if os.path.splitext(flow.request.path)[-1] in ('.jpg', '.gif'):
                     flow.response.set_content(base64.b64decode(one_px_gif_data))
 
-            if flow.request.path.endswith('1.ed0cb8b173b9bfffa25f.chunk.js'):
-                with open('js/1.ed0cb8b173b9bfffa25f.chunk.de.js', 'r') as f:
+            if flow.request.path.endswith('8.f1e991deb25eab01d184.js'):
+                with open('js/8.f1e991deb25eab01d184.de.js', 'r') as f:
                     # print(flow.response.get_text())
                     flow.response.set_text(f.read())
 
