95030207c558b416c9657063093db103f42c51f9
GreatYYX <252524yyx>
2019-02-25 21:55:54 -0800

    update inject js

--- call_killer.py
+++ call_killer.py
@@ -1,8 +1,10 @@
 import os, sys
 import base64
+import re
 
 import mitmproxy.http
 
+re_inject_js_script = re.compile(r'8\..*\.js$', re.IGNORECASE)
 
 class DNAdKiller:
 
@@ -15,9 +17,9 @@ class DNAdKiller:
             if flow.request.path.startswith('/upload/video'):
                 if os.path.splitext(flow.request.path)[-1] in ('.jpg', '.gif'):
                     flow.response.set_content(base64.b64decode(one_px_gif_data))
-
-            if flow.request.path.endswith('8.f1e991deb25eab01d184.js'):
-                with open('js/8.f1e991deb25eab01d184.de.js', 'r') as f:
+                    
+            if re_inject_js_script.search(flow.request.path):
+                with open('js/8.f56b7792ce563bfe9690.de.js', 'r') as f:
                     # print(flow.response.get_text())
                     flow.response.set_text(f.read())
 
